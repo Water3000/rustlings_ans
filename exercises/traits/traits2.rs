deea1a6c381476d982dc4165a3a8c4a9f8dc56fb
Water3000 <wangzehao1007@qq.com>
2023-07-28 23:11:42 +0800

    options1 - quiz3

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,13 +11,31 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
+    fn append_bar_flex(&mut self) -> Self;
+    fn count(&self) -> usize;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut nv = Vec::from(self);
+        nv.push("Bar".to_string());
+        nv
+    }
+
+    fn append_bar_flex(&mut self) -> Self {
+        self.push("Bar".to_string());
+    
+        let nv = self.clone();
+        nv
+    }
+
+    fn count(&self) -> usize {
+        self.len()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +46,20 @@ mod tests {
         let mut foo = vec![String::from("Foo")].append_bar();
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+
+    #[test]
+    fn is_vec_pop_eq_bar_flex() {
+        let mut foo = vec![String::from("Foo")];
+        foo.append_bar_flex();
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+
+    #[test]
+    fn is_vec_pop_eq_count() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.count(), 2);
+        assert_eq!(foo.count(), 2);
     }
 }
